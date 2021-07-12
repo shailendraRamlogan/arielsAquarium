a7750570ceea89fa7f8daf4b3da92088551f4410
shailendra42 <38821646+shailendra42@users.noreply.github.com>
2021-07-11 23:25:47 -0400

    added animation to logo
    added ease in animation to logo text on homepage

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,12 +1,14 @@
 import useStyles from './styles';
 import NavigationBar from '../navigationBar/navigationBar';
-import homeImg from '../../assets/images/home.jpg'
+import homeImg from '../../assets/images/home.jpg';
+
 const LandingPage = ()=>{
     const classes = useStyles();
     return(
         <div className={classes.body}>
             <NavigationBar/>
             <img src={homeImg} alt="home" className={classes.homeImg}/>
+            <h3 className={classes.logo}>Welcome to Ariel's Aquarium</h3>
         </div>
     );
 }
--- src/components/landingPage/styles.js
+++ src/components/landingPage/styles.js
@@ -1,5 +1,4 @@
 import {makeStyles} from '@material-ui/core/styles';
-
 export default makeStyles(() => ({
     body: {
         fontFamily: "Lexend Exa, sans-serif",
@@ -10,5 +9,27 @@ export default makeStyles(() => ({
         height: "100vh",
         
     },
+    logo:{
+        fontFamily: "'Pacifico', cursive",
+        textAlign: "center",
+        textTransform: "capitalize",
+        fontSize: "7rem",
+        color:"#ffe8fc",
+        position: "absolute",
+        width: "100%",
+        top: "30%",
+        left: "50%",
+        transform: "translate(-50%, -100%)",
+        animation: `$fadein ease 5s`,
+        animationIterationCount: 1,
+        animationFillMode: "forwards",
+    },
+
+
+    "@keyframes fadein":{
+        "0%":{ opacity: "0", },
+        "100%":{ opacity: "1", }
+    }
     
+       
 }));
